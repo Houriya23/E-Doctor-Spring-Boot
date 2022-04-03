c04220fc3be7f74e61bd83684df4c1157ebd8d09
Mami Houria <houria.mami@enicar.ucar.tn>
2022-04-03 23:25:07 +0200

    first commit

---
+++ src/main/java/com/tn/enicarthage/EDoctorProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package com.tn.enicarthage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDoctorProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDoctorProjectFinalApplication.class, args);
+	}
+
+}
